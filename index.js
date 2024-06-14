414d2ce3fdbb55da6de658c2ba0df1065d34f733
alifahmednowshad <muhammadnowshad2020@gmail.com>
2024-06-14 19:44:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,120 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+function createToken(user) {
+  const token = jwt.sign(
+    {
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+  return token;
+}
+
+function verifyToken(req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const verify = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verify?.email) {
+      return res.status(403).send("You are not authorized");
+    }
+    req.user = verify.email;
+    next();
+  } catch (error) {
+    return res.status(401).send("Unauthorized");
+  }
+}
+
+console.log(verifyToken);
+const secret = crypto.randomBytes(32).toString("base64");
+console.log(secret);
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const iLearnDB = client.db("iLearn_DB");
+    const userCollection = iLearnDB.collection("userCollection");
+    const courseCollection = iLearnDB.collection("courseCollection");
+
+    // user
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const token = createToken(user);
+      const isUserExist = await userCollection.findOne({ email: user?.email });
+      if (isUserExist?._id) {
+        return res.send({
+          status: "success",
+          message: "Login success",
+          token,
+        });
+      }
+      await userCollection.insertOne(user);
+      return res.send({ token });
+    });
+
+    app.get("/user", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/user/get/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const result = await userCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await userCollection.findOne({ email });
+      res.send(result);
+    });
+
+    app.patch("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const userData = req.body;
+      const result = await userCollection.updateOne(
+        { email },
+        { $set: userData },
+        { upsert: true }
+      );
+      res.send(result);
+    });
+
+    //course
+    app.get("/course", async (req, res) => {
+      const result = await courseCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ILearn server is connected.");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port: ${port}`);
+});
